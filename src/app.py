48122387eb3992ace7d87f2a526ecdc6f8eeda63
KostyaLKE <kostyalitvinov8@gmail.com>
2025-02-06 20:01:02 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, send_from_directory
-from src.routes import text_routes, image_routes, video_routes, youtube_routes #Импортируем все роуты
+from src.routes import text_routes, image_routes, video_routes, youtube_routes
 from src.config import Config
 
 app = Flask(__name__, static_folder='../public', static_url_path='/')
 app.config.from_object(Config)
 
 # Регистрация маршрутов
-app.register_blueprint(text_routes.text_bp, url_prefix='/api/text')  #  <-- ДОЛЖНО БЫТЬ!
-app.register_blueprint(image_routes.image_bp, url_prefix='/api/image')
+app.register_blueprint(text_routes.text_bp, url_prefix='/api/text')
+app.register_blueprint(image_routes.image_bp, url_prefix='/api/image')  # <- Убедись, что имя ПРАВИЛЬНОЕ
 app.register_blueprint(video_routes.video_bp, url_prefix='/api/video')
 app.register_blueprint(youtube_routes.youtube_bp, url_prefix='/api/youtube')
 
