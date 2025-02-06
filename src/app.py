a6662f1679fcb1cb13026eac94b8adb8f993f66b
KostyaLKE <kostyalitvinov8@gmail.com>
2025-02-06 19:42:05 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,16 +1,19 @@
 from flask import Flask, send_from_directory
-from .routes import text_routes, image_routes, video_routes, youtube_routes # Точка перед routes
-from .config import Config  # <- ИЗМЕНЕНИЕ: Точка перед config
+from src.routes import text_routes, image_routes, video_routes, youtube_routes #Импортируем все роуты
+from src.config import Config
 
 app = Flask(__name__, static_folder='../public', static_url_path='/')
 app.config.from_object(Config)
 
-# ... (регистрация маршрутов)
+# Регистрация маршрутов
+app.register_blueprint(text_routes.text_bp, url_prefix='/api/text')  #  <-- ДОЛЖНО БЫТЬ!
+app.register_blueprint(image_routes.image_bp, url_prefix='/api/image')
+app.register_blueprint(video_routes.video_bp, url_prefix='/api/video')
+app.register_blueprint(youtube_routes.youtube_bp, url_prefix='/api/youtube')
 
 @app.route('/')
 def index():
-    return send_from_directory(app.static_folder, 'index.html') # Отдаем index.html
-
+    return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
     app.run(debug=app.config['DEBUG'])
