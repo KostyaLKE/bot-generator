c7d9b17c5584021362a13c0a91cec05170cadd13
KostyaLKE <kostyalitvinov8@gmail.com>
2025-02-06 19:42:07 +0200

    Update text_routes.py

--- src/routes/text_routes.py
+++ src/routes/text_routes.py
@@ -3,5 +3,5 @@ from src.controllers import text_controller
 
 text_bp = Blueprint('text', __name__)
 
+# Убедись, что methods=['POST'] указан!
 text_bp.route('/generate', methods=['POST'])(text_controller.generate_text_from_news)
-# Другие маршруты для текста...
