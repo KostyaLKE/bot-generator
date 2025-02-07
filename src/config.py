1b7f51cc1de3cb3a2b710f6755a08804c07cb05c
KostyaLKE <kostyalitvinov8@gmail.com>
2025-02-07 14:45:34 +0200

    Delete config.py

--- src/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Config:
-    DEBUG = True  #  Для разработки
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')  # Обязательно задай!
-    OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
