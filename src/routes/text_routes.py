a86f3f710b91ecd86f1a4922bb62af03875e41e9
KostyaLKE <kostyalitvinov8@gmail.com>
2025-02-07 14:19:21 +0200

    Delete text_routes.py

--- src/routes/text_routes.py
+++
@@ -1,9 +0,0 @@
-# bot-generator-main/src/routes/text_routes.py
-
-from flask import Blueprint
-from src.controllers import text_controller
-
-text_bp = Blueprint('text', __name__)
-
-# Убедись, что methods=['POST'] указан!
-text_bp.route('/generate', methods=['POST'])(text_controller.generate_text_from_news)
