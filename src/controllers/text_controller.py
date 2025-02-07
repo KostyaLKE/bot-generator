7726524acc50fd7fb630cffd3ea9009543e0ec0f
KostyaLKE <kostyalitvinov8@gmail.com>
2025-02-07 13:15:47 +0200

    Update text_controller.py

--- src/controllers/text_controller.py
+++ src/controllers/text_controller.py
@@ -1,22 +1,14 @@
-# bot-generator-main/src/controllers/text_controller.py
+# src/controllers/text_controller.py
 
 from flask import request, jsonify, Blueprint
-from src.services.text_generation.text_service import text_service  #  Импортируем text_service
+from src.services.text_generation.simple_test import test_openai  # Импортируем новую функцию
 
 
 def generate_text_from_news():
     try:
-        # data = request.get_json() #Устарело
-        # Вместо request.get_json()
-        news_text = request.form.get('newsText') # Текст
-        social_networks = request.form.getlist('socialNetworks') # Список соц. сетей
-        other_name = request.form.get('otherName')
-        action = request.form.get('action')
-
-        #Тут оставим вызов генерации текста
-        if action == "generate_text":
-            generated_texts = text_service.generate_text_for_social_media(news_text, social_networks, other_name)
-            return jsonify(generated_texts), 200
+        #  Временно используем простую функцию
+        result = test_openai()
+        return jsonify({"result": result}), 200
 
 
     except Exception as e:
