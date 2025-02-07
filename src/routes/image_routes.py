f9edbad11185557a097202a315e5f1da082c9e1c
KostyaLKE <kostyalitvinov8@gmail.com>
2025-02-07 14:19:19 +0200

    Delete image_routes.py

--- src/routes/image_routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-# from src.controllers import image_controller  # Раскомментируй, когда создашь контроллер
-
-image_bp = Blueprint('image', __name__)  # <- СОЗДАЙ Blueprint с именем image_bp
-
-# @image_bp.route('/generate', methods=['POST']) # Раскомментируй и создай функцию, когда будешь готов
-# def generate_image():
-#     # ... Логика генерации изображения ...
-#     pass
