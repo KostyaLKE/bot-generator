76d0efc12daf10e497f0d81e47d5cab6edba9ab7
KostyaLKE <kostyalitvinov8@gmail.com>
2025-02-07 14:20:04 +0200

    Update config.py

--- src/config.py
+++ src/config.py
@@ -3,9 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-class Config:  # <- Класс Config с большой буквы
-    DEBUG = True
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')
+class Config:
+    DEBUG = True  #  Для разработки
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key')  # Обязательно задай!
     OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
-    STABILITY_API_KEY = os.environ.get('STABILITY_API_KEY')
-    # ... другие настройки
